f342389ae674c2d76d5496f8da1f4139131e5d78
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-05-15 17:09:37 +0530

    Create placeOrderController.js

---
+++ controllers/placeOrderController.js
@@ -0,0 +1,18 @@
+import { placeOrderAndUploadFile } from "../services/sftpService";
+
+
+export const  appendData=async (req,res)=> {
+    const orderJson = req.body
+    console.log(orderJson)
+
+try {
+    const data = await placeOrderAndUploadFile(orderJson);
+    
+  
+    res.json(data);
+    console.log(data)
+  } catch (err) {
+    console.error('Error uploading order', err.message);
+    res.status(500).json({ error: 'Failed to upload order' });
+  }
+}
