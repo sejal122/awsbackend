d737ed9ba65cb777714cb19bb5876b10badaa8df
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-05-15 16:33:36 +0530

    Update sftpService.js

--- services/sftpService.js
+++ services/sftpService.js
@@ -1,7 +1,9 @@
 const Client = require('ssh2-sftp-client');
 const { parseCSV } = require('../utils/csvParser');
 require('dotenv').config();
-
+const fs = require('fs');
+const path = require('path');
+const { Parser } = require('json2csv');
 const sftp = new Client();
 
 async function fetchAndParseCSV() {
@@ -37,4 +39,38 @@ async function fetchAndParseProductsCSV() {
   return parseCSV(csvText);
 }
 
-module.exports = { fetchAndParseCSV,fetchAndParseProductsCSV };
+async function placeOrderAndUploadFile(orderJson) {
+  try{
+    await sftp.connect({
+      host: process.env.SERVER_IP,
+      port: process.env.SERVER_PORT,
+      username: process.env.SERVER_USER,
+      password: process.env.SERVER_PASS,
+    });
+   
+        const json2csvParser = new Parser();
+      const csv = json2csvParser.parse(orderJson);
+      const fileName = `pendingOrders.csv`;
+      const tempPath = path.join(__dirname, fileName);
+      const targetFolder = '/DIR_MAGICAL/DIR_MAGICAL_Satara/SO'; // Change this if needed
+      const finalPath = path.join(targetFolder, fileName);
+  console.log(tempPath)
+  
+    console.log(finalPath)
+    fs.writeFileSync(tempPath, csv, (err) => {
+      if (err) {
+        console.error('Error writing temp CSV file:', err);
+        return;
+      }
+    
+      console.log(`CSV file created at: ${tempPath}`);
+  
+    });
+  await sftp.put(tempPath, finalPath);
+  await sftp.end();
+  }catch (err) {
+    console.error('SFTP Error:', err);
+  }
+
+}
+module.exports = { fetchAndParseCSV,fetchAndParseProductsCSV ,placeOrderAndUploadFile};
