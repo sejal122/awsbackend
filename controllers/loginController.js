422af3147c01965d07e883baa74e4ccafaad12c2
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-05-16 13:30:30 +0530

    Update loginController.js

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,17 +1,17 @@
 const {fetchAndParseCSV} =require('../services/sftpService')
 const  handleLogin=async (req,res)=> {
 const {phone}=req.body
+console.log("phone")
 console.log(phone)
 try {
     let dealers = await fetchAndParseCSV();
-    console.log("dealers") // Should return array of dealer objects
-    console.log(dealers)
-    console.log(typeof dealers)
+    // console.log("dealers") // Should return array of dealer objects
+    // console.log(dealers)
+    //console.log(typeof dealers)
     
      dealers=Object.values(dealers);
      const dealer = dealers.find(d => d.Phone_number === phone);
-        console.log('matching dealer')
-    console.log(dealer)
+console.log(dealer)
     if (dealer) {
       res.json({ success: true, dealer });
     } else {
