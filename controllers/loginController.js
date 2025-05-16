9db8ddc292d6a8c1f9e4f73d869ce0e616a4ee8c
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-05-16 12:39:57 +0530

    Add files via upload

---
+++ controllers/loginController.js
@@ -0,0 +1,26 @@
+const {verifyDealer} =require('../services/sftpService')
+const  handleLogin=async (req,res)=> {
+const {phone}=req.body
+console.log(phone)
+try {
+    const dealers = await verifyDealer(); // Should return array of dealer objects
+    console.log(dealers)
+    console.log(typeof dealers)
+    const dealer = dealers.find(d => d.phone === phone);
+
+    if (dealer) {
+      res.json({ success: true, dealer });
+    } else {
+      res.status(401).json({ success: false, message: 'Phone number not registered' });
+    }
+  } catch (err) {
+    console.error('Error fetching dealers:', err);
+    res.status(500).json({ success: false, message: 'Server error' });
+  }
+
+
+
+
+
+}
+module.exports={handleLogin}
