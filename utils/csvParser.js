c08d191e2bb3c4758b877085a94cb958868b41b5
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-05-17 14:39:44 +0530

    Update csvParser.js

--- utils/csvParser.js
+++ utils/csvParser.js
@@ -30,25 +30,34 @@ function parseCSV(csvString) {
   if (lines.length < 2) return [];
 
   // Clean header line
+  // const headers = lines[0].split(',').map(header =>
+  //   header.trim().replace(/^"+|"+$/g, '')
+  // );
   const headers = lines[0].split(',').map(header =>
-    header.trim().replace(/^"+|"+$/g, '')
+    header.trim()
   );
-
-  const data = lines.slice(1).map(line => {
-    const values = line.split(',').map(val =>
-      val.trim().replace(/^"+|"+$/g, '')
-    );
+  // const data = lines.slice(1).map(line => {
+  //   const values = line.split(',').map(val =>
+  //     val.trim().replace(/^"+|"+$/g, '')
+  //   );
+    const data = lines.slice(1).map(line => {
+      const values = line.split(',').map(val =>
+        val.trim()
+      );
 
     const obj = {};
     headers.forEach((key, index) => {
       obj[key] = values[index] || '';
     });
- console.log('obj')
-      console.log(obj)
+    console.log( obj)
+    
+    
     return obj;
+   
   });
 
   return data;
+  
 }
 
 module.exports = {parseCSV};
