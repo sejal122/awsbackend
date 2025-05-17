a4da22196062cf4d9545fb82fdc1ed0fb8b845a5
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-05-17 14:35:07 +0530

    Update sftpService.js

--- services/sftpService.js
+++ services/sftpService.js
@@ -31,7 +31,7 @@ async function fetchAndParseProductsCSV() {
     password: process.env.SERVER_PASS,
   });
 
-  const fileBuffer = await sftp.get('/DIR_MAGICAL/DIR_MAGICAL_Satara/Price/SAP PRICE LIST TEMPLET - 18.03.25.csv');
+  const fileBuffer = await sftp.get('/DIR_MAGICAL/DIR_MAGICAL_Satara/Price/HANA TEMPLET GROUP WISE.csv');
   console.log(fileBuffer)
   await sftp.end();
 
