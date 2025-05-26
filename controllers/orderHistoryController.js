a3f862a0fabf329bd959b079dbf65ff8df712e40
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-05-26 10:26:01 +0530

    Add files via upload

---
+++ controllers/orderHistoryController.js
@@ -0,0 +1,14 @@
+const { fetchAndParseOrderHistoryCSV } = require('../services/sftpService');
+const getOrderHistory = async (req, res) => {
+    try {
+        const data = await fetchAndParseOrderHistoryCSV();
+     
+        res.json(data);
+        console.log(data)
+      } catch (err) {
+        console.error('Error fetching order history:', err.message);
+        res.status(500).json({ error: 'Failed to fetch history data' });
+      }
+    
+}
+module.exports = { getOrderHistory };
