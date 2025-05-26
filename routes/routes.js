a7b3665104a43990ef1a8d8aa14a549762e678c6
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-05-26 10:29:31 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -14,12 +14,12 @@ const { appendData } = require('../controllers/placeOrderController');
 const {handleLogin} = require('../controllers/loginController')
 const {getOutstanding}= require('../controllers/outstandingController')
 const {getSubdealerData}=require('../controllers/subDealerController')
-const {getOutstanding}= require('../controllers/outstandingController')
+const {getOrderHistory} =require('../controllers/orderHistoryController')
 router.get('/dealers', getDealers);
 router.get('/products', getProducts);
 router.post('/upload-csv',appendData)
 router.post('/login',handleLogin)
 router.get('/outstanding',getOutstanding)
 router.get('/sub-dealers',getSubdealerData)
-router.get('/outstanding',getOutstanding)
+router.get('/orderHistory',getOrderHistory)
 module.exports = router;
