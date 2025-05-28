d4e0455cf7a2b882c56c6c6e6bb52993bab78475
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-05-28 23:34:29 +0530

    Update orderHistoryController.js

--- controllers/orderHistoryController.js
+++ controllers/orderHistoryController.js
@@ -4,7 +4,7 @@ const getOrderHistory = async (req, res) => {
         const data = await fetchAndParseOrderHistoryCSV();
      
         res.json(data);
-        console.log(data)
+       // console.log(data)
       } catch (err) {
         console.error('Error fetching order history:', err.message);
         res.status(500).json({ error: 'Failed to fetch history data' });
