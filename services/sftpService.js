c5f7bf66a3fc9e72d0b0ffccf3c1e57955f0a56a
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-05-30 23:57:00 +0530

    Update sftpService.js

--- services/sftpService.js
+++ services/sftpService.js
@@ -101,6 +101,7 @@ async function fetchAndParseProductsCSV() {
 const Papa = require('papaparse');
 
 async function placeOrderAndUploadFile(orderJson) {
+  const orderJson = Array.isArray(orderJsonInput) ? orderJsonInput : [orderJsonInput];
    const sftp = new Client();
 
   try {
