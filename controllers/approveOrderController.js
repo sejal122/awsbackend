6fd22d2f9762e14456c00f111a36b6b514e54731
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-06-06 16:22:47 +0530

    Add files via upload

---
+++ controllers/approveOrderController.js
@@ -0,0 +1,25 @@
+//import { placeOrderAndUploadFile } from "../services/sftpService";
+
+const { approveOrderAndUploadFile } = require('../services/sftpService');
+
+ const  approveOrder=async (req,res)=> {
+
+    const {  doc_number,approvedOrders } = req.body;
+    console.log(approvedOrders)
+    console.log(doc_number)
+
+try {
+    const data = await approveOrderAndUploadFile(doc_number,approvedOrders);
+  
+  
+    res.json(data);
+    console.log(data)
+  } catch (err) {
+    console.error('Error approving order', err.message);
+ if (!res.headersSent) {
+     res.status(500).json({ error:  'Error approving order' });
+    }
+    
+  }
+}
+module.exports = { approveOrder };
