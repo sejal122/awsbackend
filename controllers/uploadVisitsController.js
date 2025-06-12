493ec8f4f8a84a90ed57c8dfec8e8cddb7c82233
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-06-12 12:43:34 +0530

    Add files via upload

---
+++ controllers/uploadVisitsController.js
@@ -0,0 +1,14 @@
+const uploadVisits=async(req,res)=>{
+    try {
+        const data = await uploadVisitsCSV();
+        
+        await cache.set('dealers', data);
+        res.json(data);
+        console.log(data)
+      } catch (err) {
+        console.error('Error fetching dealers:', err.message);
+        res.status(500).json({ error: 'Failed to fetch dealer data' });
+      }
+}
+
+module.exports = { uploadVisits };
