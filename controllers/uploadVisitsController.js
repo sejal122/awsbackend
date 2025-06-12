3df8fde4d039eae8a8288ca0a202ee4e5ba3dbd4
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-06-12 12:47:44 +0530

    Update uploadVisitsController.js

--- controllers/uploadVisitsController.js
+++ controllers/uploadVisitsController.js
@@ -1,13 +1,12 @@
 const uploadVisits=async(req,res)=>{
     try {
-        const data = await uploadVisitsCSV();
-        
-        await cache.set('dealers', data);
+        const visitJson = req.body
+        const data = await uploadVisitsCSV(visitJson);
         res.json(data);
         console.log(data)
       } catch (err) {
-        console.error('Error fetching dealers:', err.message);
-        res.status(500).json({ error: 'Failed to fetch dealer data' });
+        console.error('Error uploading visits:', err.message);
+        res.status(500).json({ error: 'Failed to upload visits' });
       }
 }
 
