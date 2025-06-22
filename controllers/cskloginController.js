f65e92199a06a750a5c824e0667cb0574972a6d7
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-06-22 11:42:44 +0530

    Add files via upload

---
+++ controllers/cskloginController.js
@@ -0,0 +1,34 @@
+const {fetchAndParseCSV} =require('../services/sftpService')
+const  csklogin=async (req,res)=> {
+const {identifier}=req.body
+const phone=identifier
+console.log("phone")
+console.log(req.body)
+try {
+    let dealers = await fetchAndParseCSV();
+    console.log(dealers)
+    // console.log("dealers") // Should return array of dealer objects
+    // console.log(dealers)
+    //console.log(typeof dealers)
+    
+     dealers=Object.values(dealers);
+    // const dealer = dealers.find(d => d.Phone_number === phone);
+    localStorage.setItem('allDealers', JSON.stringify(data));
+    const dealer = dealers.find(d => d['Telephone 1'] === phone);
+console.log(dealer)
+    if (dealer) {
+      res.json({ success: true, dealer });
+    } else {
+      res.status(401).json({ success: false, message: 'Phone number not registered' });
+    }
+  } catch (err) {
+    console.error('Error fetching dealers:', err);
+    res.status(500).json({ success: false, message: 'Server error' });
+  }
+
+
+
+
+
+}
+module.exports={csklogin}
