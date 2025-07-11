e3355823285638d4505e65f9a94db1a76dd8901d
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-07-11 13:47:37 +0530

    Update replacependingorder.js

--- controllers/replacependingorder.js
+++ controllers/replacependingorder.js
@@ -1,4 +1,4 @@
-const { replacePendingOrder } = require('../services/sftpService');
+const { replacePendingOrder ,replacePendingOrderShrirampur} = require('../services/sftpService');
 const  replacependingordercontroller=async (req,res)=> {
 
     const { purch_no_c,updatedOrderArray } = req.body;
@@ -21,4 +21,27 @@ try {
     
   }
 }
-module.exports = { replacependingordercontroller };
+
+const  replacependingordercontrollerShrirampur=async (req,res)=> {
+
+    const { purch_no_c,updatedOrderArray } = req.body;
+    console.log(updatedOrderArray)
+    console.log(purch_no_c)
+
+try {
+    
+ 
+    const data = await replacePendingOrderShrirampur(purch_no_c,updatedOrderArray);
+  
+  
+    res.json(data);
+    console.log(data)
+  } catch (err) {
+    console.error('Error updating order', err.message);
+ if (!res.headersSent) {
+     res.status(500).json({ error:  'Error updating order' });
+    }
+    
+  }
+}
+module.exports = { replacependingordercontroller ,replacependingordercontrollerShrirampur};
