648819c6f230517f78dd5b011f30e475e2b9f5ee
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-07-11 14:13:15 +0530

    Update cskloginController.js

--- controllers/cskloginController.js
+++ controllers/cskloginController.js
@@ -1,4 +1,4 @@
-const {fetchAndParseCSV} =require('../services/sftpService')
+const {fetchAndParseCSV,fetchAndParseCSVShrirampur} =require('../services/sftpService')
 const  csklogin=async (req,res)=> {
 const {identifier}=req.body
 const phone=identifier
@@ -25,10 +25,33 @@ console.log(dealer)
     console.error('Error fetching dealers:', err);
     res.status(500).json({ success: false, message: 'Server error' });
   }
-
-
-
-
-
 }
-module.exports={csklogin}
+
+const  cskloginShrirampur=async (req,res)=> {
+const {identifier}=req.body
+const phone=identifier
+console.log("phone")
+console.log(req.body)
+try {
+    let dealers = await fetchAndParseCSVShrirampur();
+    console.log(dealers)
+    // console.log("dealers") // Should return array of dealer objects
+    // console.log(dealers)
+    //console.log(typeof dealers)
+    
+     dealers=Object.values(dealers);
+    // const dealer = dealers.find(d => d.Phone_number === phone);
+   
+    const dealer = dealers.filter(d => d['Telephone 1'] === phone);
+console.log(dealer)
+    if (dealer) {
+      res.json({ success: true, dealer });
+    } else {
+      res.status(401).json({ success: false, message: 'Phone number not registered' });
+    }
+  } catch (err) {
+    console.error('Error fetching dealers:', err);
+    res.status(500).json({ success: false, message: 'Server error' });
+  }
+}
+module.exports={csklogin,cskloginShrirampur}
