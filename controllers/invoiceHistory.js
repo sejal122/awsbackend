ebf353f43db6b496eb6095cc1c2310e82d101564
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-07-11 14:14:57 +0530

    Update invoiceHistory.js

--- controllers/invoiceHistory.js
+++ controllers/invoiceHistory.js
@@ -1,4 +1,4 @@
-const { fetchAndParseInvoiceHistory } = require('../services/sftpService');
+const { fetchAndParseInvoiceHistory,fetchAndParseInvoiceHistoryShrirampur } = require('../services/sftpService');
 const getInvoiceHistory = async (req, res) => {
     try {
         const data = await fetchAndParseInvoiceHistory();
@@ -11,4 +11,17 @@ const getInvoiceHistory = async (req, res) => {
       }
     
 }
-module.exports = { getInvoiceHistory };
+
+const getInvoiceHistoryShrirampur = async (req, res) => {
+    try {
+        const data = await fetchAndParseInvoiceHistoryShrirampur();
+     
+        res.json(data);
+        console.log(data)
+      } catch (err) {
+        console.error('Error fetching invoice history:', err.message);
+        res.status(500).json({ error: 'Failed to fetch invoice history data' });
+      }
+    
+}
+module.exports = { getInvoiceHistory ,getInvoiceHistoryShrirampur};
