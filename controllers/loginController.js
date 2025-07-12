25df04f0c029b872a7cc9dd8dd9ed46f89244be6
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-07-12 13:33:26 +0530

    Update loginController.js

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,4 +1,4 @@
-const {fetchAndParseCSV,fetchAndParseCSVShrirampur} =require('../services/sftpService')
+const {fetchAndParseCSV,fetchAndParseCSVShrirampur,fetchAndParseCSVBaramati} =require('../services/sftpService')
 const  handleLogin=async (req,res)=> {
 const {identifier}=req.body
 const phone=identifier
@@ -62,4 +62,36 @@ try {
   }
 
 }
-module.exports={handleLogin,handleLoginShrirampur}
+
+const  handleLoginBaramati=async (req,res)=> {
+const {identifier}=req.body
+const phone=identifier
+console.log(phone)
+console.log(req.body)
+try {
+    let dealers = await fetchAndParseCSVBaramati();
+   // console.log(dealers)
+    // console.log("dealers") // Should return array of dealer objects
+    // console.log(dealers)
+    //console.log(typeof dealers)
+   
+     dealers=Object.values(dealers);
+    // const dealer = dealers.find(d => d.Phone_number === phone);
+   
+    const dealer = dealers.find(d => d['Telephone 1'] === phone);
+//console.log(dealer)
+    if (dealer) {
+      res.json({ success: true, dealer });
+    } else {
+      res.status(401).json({ success: false, message: 'Phone number not registered' });
+    }
+  } catch (err) {
+    console.error('Error fetching dealers:', err);
+    if (!res.headersSent) {
+      res.status(500).json({ success: false, message: 'Server error' });
+    }
+   
+  }
+
+}
+module.exports={handleLogin,handleLoginShrirampur,handleLoginBaramati}
