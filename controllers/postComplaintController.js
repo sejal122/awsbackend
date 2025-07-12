ab06130e8f0ee46b206805bc2653200374dd292a
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-07-12 13:49:23 +0530

    Update postComplaintController.js

--- controllers/postComplaintController.js
+++ controllers/postComplaintController.js
@@ -1,4 +1,4 @@
-const { saveComplaintToSFTP,saveComplaintToSFTPShrirampur } = require('../services/sftpService');
+const { saveComplaintToSFTP,saveComplaintToSFTPShrirampur,saveComplaintToSFTPBaramati} = require('../services/sftpService');
 
 const postComplaint = async (req, res) => {
   try {
@@ -57,4 +57,32 @@ const postComplaintShrirampur = async (req, res) => {
   }
 };
 
-module.exports = { postComplaint ,postComplaintShrirampur};
+const postComplaintBaramati = async (req, res) => {
+  try {
+    const { ID, Name, date } = req.body;
+    const file = req.file;
+
+    if (!file) {
+      return res.status(400).json({ error: 'Photo file is missing' });
+    }
+
+    const filePath = file.path;
+    const fileName = file.originalname;
+
+    const data = await saveComplaintToSFTPBaramati({
+      ID,
+      Name,
+      date,
+      filePath,
+      fileName
+    });
+
+    res.json({ message: '✅ Complaint uploaded successfully', data });
+    console.log('📸 Complaint uploaded:', data);
+  } catch (err) {
+    console.error('❌ Error uploading complaint:', err.message);
+    res.status(500).json({ error: 'Failed to upload complaint' });
+  }
+};
+
+module.exports = { postComplaint ,postComplaintShrirampur,postComplaintBaramati};
