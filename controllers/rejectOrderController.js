2ab19107835bb62044414741b39280b07a11a45e
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-07-12 13:51:28 +0530

    Update rejectOrderController.js

--- controllers/rejectOrderController.js
+++ controllers/rejectOrderController.js
@@ -1,7 +1,7 @@
 
 //import { placeOrderAndUploadFile } from "../services/sftpService";
 
-const { RejectOrderAndUploadFile,RejectOrderAndUploadFileShrirampur } = require('../services/sftpService');
+const { RejectOrderAndUploadFile,RejectOrderAndUploadFileShrirampur,RejectOrderAndUploadFileBaramati} = require('../services/sftpService');
 
  const  rejectOrder=async (req,res)=> {
 
@@ -41,4 +41,24 @@ try {
     
   }
 }
-module.exports = { rejectOrder ,rejectOrderShrirampur};
+
+ const  rejectOrderBaramati=async (req,res)=> {
+
+    const {  doc_number,approvedHistoryFormat } = req.body;
+    console.log(approvedHistoryFormat)
+    console.log(doc_number)
+
+try {
+    const data = await RejectOrderAndUploadFileBaramati(doc_number,approvedHistoryFormat);
+   res.status(200).json(data);
+  
+  } catch (err) {
+    console.error('Error rejecting order', err.message);
+ 
+ if (!res.headersSent) {
+     res.status(500).json({ error:  'Error rejecting order' });
+    }
+    
+  }
+}
+module.exports = { rejectOrder ,rejectOrderShrirampur,rejectOrderBaramati};
