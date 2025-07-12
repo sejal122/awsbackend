3449cd542aaef00cd5e93b397de79319b647b0e8
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-07-12 13:48:16 +0530

    Update placeOrderController.js

--- controllers/placeOrderController.js
+++ controllers/placeOrderController.js
@@ -1,6 +1,6 @@
 //import { placeOrderAndUploadFile } from "../services/sftpService";
 
-const { placeOrderAndUploadFile ,placeOrderAndUploadFileShrirampur } = require('../services/sftpService');
+const { placeOrderAndUploadFile ,placeOrderAndUploadFileShrirampur,placeOrderAndUploadFileBaramati} = require('../services/sftpService');
 
  const  appendData=async (req,res)=> {
   console.log('hi')
@@ -42,4 +42,25 @@ try {
     
   }
 }
-module.exports = { appendData ,appendDataShrirampur};
+
+
+ const  appendDataBaramati=async (req,res)=> {
+  console.log('hi')
+    const orderJson = req.body
+    console.log(orderJson)
+
+try {
+    const data = await placeOrderAndUploadFileBaramati(orderJson);
+  
+  
+    res.json(data);
+    console.log(data)
+  } catch (err) {
+    console.error('Error uploading orderrrrrrrrrrr', err.message);
+ if (!res.headersSent) {
+     res.status(500).json({ error:  'Error uploading order' });
+    }
+    
+  }
+}
+module.exports = { appendData ,appendDataShrirampur,appendDataBaramati};
