7b960c7ec948caf3a2d64dbb6c0be4f7ed94e541
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-07-12 13:34:18 +0530

    Update orderHistoryController.js

--- controllers/orderHistoryController.js
+++ controllers/orderHistoryController.js
@@ -1,4 +1,4 @@
-const { fetchAndParseOrderHistoryCSV,fetchAndParseOrderHistoryCSVShrirampur } = require('../services/sftpService');
+const { fetchAndParseOrderHistoryCSV,fetchAndParseOrderHistoryCSVShrirampur,fetchAndParseOrderHistoryCSVBaramati } = require('../services/sftpService');
 const getOrderHistory = async (req, res) => {
     try {
         const data = await fetchAndParseOrderHistoryCSV();
@@ -24,4 +24,17 @@ const getOrderHistoryShrirampur = async (req, res) => {
       }
     
 }
-module.exports = { getOrderHistory ,getOrderHistoryShrirampur};
+
+const getOrderHistoryBaramati = async (req, res) => {
+    try {
+        const data = await fetchAndParseOrderHistoryCSVBaramati();
+     
+        res.json(data);
+       // console.log(data)
+      } catch (err) {
+        console.error('Error fetching order history:', err.message);
+        res.status(500).json({ error: 'Failed to fetch history data' });
+      }
+    
+}
+module.exports = { getOrderHistory ,getOrderHistoryShrirampur,getOrderHistoryBaramati};
