d72b753682baada1bb949a2a1dbbbbb727cd66d3
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-08-08 13:47:20 +0530

    Update getleadsController.js

--- controllers/getleadsController.js
+++ controllers/getleadsController.js
@@ -1,12 +1,6 @@
 const { fetchLeadsCSV } = require('../services/sftpService');
 const getLeads = async (req, res) => {
-    let dealers = await cache.get('dealers');
-    if (dealers) {
-      // If data is cached, send it
-      console.log('Returning cached dealers data');
-      console.log(res.json(dealers))
-      return res.json(dealers);
-    }else{
+    
       try {
         const data = await fetchLeadsCSV();
         
@@ -17,7 +11,7 @@ const getLeads = async (req, res) => {
         console.error('Error fetching dealers:', err.message);
         res.status(500).json({ error: 'Failed to fetch dealer data' });
       }
-    }
+    
    
   };
   module.exports = { getLeads };
