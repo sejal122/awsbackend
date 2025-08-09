fb8c82bf5ce0ee628e0552d262738f73b9529de7
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-08-09 12:17:12 +0530

    Update sftpService.js

--- services/sftpService.js
+++ services/sftpService.js
@@ -23,7 +23,7 @@ async function fetchLeadsCSV() {
 
   const csvText = fileBuffer.toString('utf-8');
  console.log(csvText);
-  return parseCSVstatus(csvText);
+  return parseCSV(csvText);
 }
 
 async function uploadLeadCSV(lead) {
