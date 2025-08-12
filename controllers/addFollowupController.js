fb7e4a61a8d14bfa573f844e8020d193ee1e7081
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-08-12 15:33:56 +0530

    Update addFollowupController.js

--- controllers/addFollowupController.js
+++ controllers/addFollowupController.js
@@ -3,6 +3,7 @@ const addFollowupController = async (req, res) => {
     const { leadID, followup } = req.body;
 console.log("------------ id ----------")
     console.log(leadID);
+    console.log("followup");
     if (!leadID || !followup) {
       return res.status(400).json({ error: 'leadId and followup are required' });
     }
