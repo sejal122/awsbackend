87cf86c9bebfbf1450cdfe53b82195bc5c7b5ca2
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-08-16 23:11:36 +0530

    Add files via upload

---
+++ controllers/gothaVisitsController.js
@@ -0,0 +1,16 @@
+const { uploadGothaVisitCSV } = require('../services/sftpService');
+const GothaController=async(req,res)=>{
+    try {
+        console.log("---------------------")
+        console.log(req.body);
+        const visitJson = req.body
+        const data = await uploadGothaVisitCSV(visitJson);
+        res.json(data);
+        console.log(data)
+      } catch (err) {
+        console.error('Error uploading leads:', err.message);
+        res.status(500).json({ error: 'Failed to upload leads' });
+      }
+}
+
+module.exports = { GothaController };
