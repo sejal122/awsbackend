743a8481b99becff353e86314a86cee6038fb0ef
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-08-18 17:28:28 +0530

    Add files via upload

---
+++ controllers/addOpinionController.js
@@ -0,0 +1,19 @@
+const { addGothaFollowupCSV } = require('../services/sftpService');
+const addFollowupController = async (req, res) => {
+    const { leadID, followup } = req.body;
+console.log("------------ id ----------")
+    console.log(leadID);
+    console.log("opinion");
+    if (!leadID || !followup) {
+      return res.status(400).json({ error: 'gothaId and opinion are required' });
+    }
+    try {
+       const data = await addGothaFollowupCSV(leadID, followup);
+        return res.json({ success: true, lead: data });
+        console.log(data);
+      } catch (err) {
+        console.error('Error updating opinion:', err.message);
+        res.status(500).json({ error: 'Failed to update gotha data' });
+      }
+  };
+  module.exports = { addFollowupController };
