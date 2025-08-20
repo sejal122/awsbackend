d935a7cdeb7ff57a5189faa157d435386d247a02
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-08-20 15:31:13 +0530

    Update addFollowupController.js

--- controllers/addFollowupController.js
+++ controllers/addFollowupController.js
@@ -1,4 +1,4 @@
-const { addFollowupCSV } = require('../services/sftpService');
+const { addFollowupCSV,addFollowupBaramatiCSV } = require('../services/sftpService');
 const addFollowupController = async (req, res) => {
     const { leadID, followup } = req.body;
 console.log("------------ id ----------")
@@ -16,4 +16,22 @@ console.log("------------ id ----------")
         res.status(500).json({ error: 'Failed to update leads data' });
       }
   };
-  module.exports = { addFollowupController };
+
+const addFollowupControllerBaramati = async (req, res) => {
+    const { leadID, followup } = req.body;
+console.log("------------ id ----------")
+    console.log(leadID);
+    console.log("followup");
+    if (!leadID || !followup) {
+      return res.status(400).json({ error: 'leadId and followup are required' });
+    }
+    try {
+       const data = await addFollowupBaramatiCSV(leadID, followup);
+        return res.json({ success: true, lead: data });
+        console.log(data);
+      } catch (err) {
+        console.error('Error updating leads:', err.message);
+        res.status(500).json({ error: 'Failed to update leads data' });
+      }
+  };
+  module.exports = { addFollowupController,addFollowupControllerBaramati };
