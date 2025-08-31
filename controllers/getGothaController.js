63e0481a3a14adda63d1c8acb187abc41828b50c
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-08-31 12:46:12 +0530

    Update getGothaController.js

--- controllers/getGothaController.js
+++ controllers/getGothaController.js
@@ -1,4 +1,4 @@
-const { fetchGothaCSV } = require('../services/sftpService');
+const { fetchGothaCSV,fetchGothaCSVBaramati } = require('../services/sftpService');
 const getGotha = async (req, res) => {
    
   
@@ -15,4 +15,20 @@ const getGotha = async (req, res) => {
     
    
   };
-  module.exports = { getGotha };
+const getGothaBaramati = async (req, res) => {
+   
+  
+      try {
+        const data = await fetchGothaCSVBaramati();
+        
+       
+        res.json(data);
+        console.log(data)
+      } catch (err) {
+        console.error('Error fetching gotha details:', err.message);
+        res.status(500).json({ error: 'Failed to fetch gotha data' });
+      }
+    
+   
+  };
+  module.exports = { getGotha ,getGothaBaramati};
