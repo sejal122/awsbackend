634631c9976c1aebe8043437cfcb9a8dd62ea627
SEJAL <56817477+sejal122@users.noreply.github.com>
2025-08-31 12:44:59 +0530

    Update addOpinionController.js

--- controllers/addOpinionController.js
+++ controllers/addOpinionController.js
@@ -1,4 +1,4 @@
-const { addGothaFollowupCSV } = require('../services/sftpService');
+const { addGothaFollowupCSV ,addGothaFollowupCSVBaramati} = require('../services/sftpService');
 const addOpinionController = async (req, res) => {
     const { leadID, followup } = req.body;
 console.log("------------ id ----------")
@@ -16,4 +16,22 @@ console.log("------------ id ----------")
         res.status(500).json({ error: 'Failed to update gotha data' });
       }
   };
-  module.exports = { addOpinionController };
+
+const addOpinionControllerBaramati = async (req, res) => {
+    const { leadID, followup } = req.body;
+console.log("------------ id ----------")
+    console.log(leadID);
+    console.log(followup);
+    if (!leadID || !followup) {
+      return res.status(400).json({ error: 'gothaId and opinion are required' });
+    }
+    try {
+       const data = await addGothaFollowupCSVBaramati(leadID, followup);
+        return res.json({ success: true, lead: data });
+        console.log(data);
+      } catch (err) {
+        console.error('Error updating opinion:', err.message);
+        res.status(500).json({ error: 'Failed to update gotha data' });
+      }
+  };
+  module.exports = { addOpinionController,addOpinionControllerBaramati };
